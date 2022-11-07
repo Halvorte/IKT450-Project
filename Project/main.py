17ccd3119723eda8ee1d3ce49dd33313b24e138a
Erlend Siqveland <erlendsi@uia.no>
2022-11-07 09:40:33 +0100

    Created a dict containting all dataframes

--- Project/main.py
+++ Project/main.py
@@ -1,20 +1,22 @@
 # Import
 import pandas as pd
 from IPython.display import display
+import os
 import matplotlib.pyplot as plt
-#from pandas.table.plotting import table
+import time
+
+# from pandas.table.plotting import table
 
 
 # Function for sliding a window over dataframe to select window to train model on
 def sliding_window(dataframe, window_size):
     # Loop to move the window by 1
-    #start = 1
-    #to = window_size + 1
+    # start = 1
+    # to = window_size + 1
     start = 0
     to = window_size
 
     for i in range(len(dataframe) - window_size):
-
         # Define the window
         window = dataframe.iloc[start:to]
         display(window)
@@ -30,13 +32,40 @@ def sliding_window(dataframe, window_size):
     return window
 
 
+def f(x, y):
+    if y - x > 0:
+        return 1
+    else:
+        return 0
+
+
+def calulateOpenClose(df):
+    for item, value in df.items():
+        result = [f(x, y) for x, y in zip(value["Open"], value["Close"])]
+        value["closeMinusOpen"] = result
+    return df
+
+
+def createDataframes(files):
+    dfs = {f'{os.path.basename(file)}_df': pd.read_csv(file, usecols=["Date", "Open", "Close"]) for file in files}
+    return dfs
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Import the data
-    stock_df = pd.read_csv('stock_data/AAPL.csv')
-    stock_amzn = pd.read_csv('stock_data/AMZN.csv')
-    stock_orcl = pd.read_csv('stock_data/ORCL.csv')
-    display(stock_df)
+    print('Fetching dataset')
+    st = time.time()
+    path = os.path.join(os.getcwd(), 'stock_data', 'price', 'raw')
+    files = [os.path.join(path, i) for i in os.listdir(path) if os.path.isfile(os.path.join(path, i))]
+    dictOfDfs = createDataframes(files)
+    dictOfDfs = calulateOpenClose(dictOfDfs)
+    et = time.time()
+    print('Dataset ready in ' + str(et - st) + 's')
 
-    yo = sliding_window(stock_df, 5)
+    # stock_aapl = pd.read_csv('stock_data/raw/AAPL.csv', usecols=["Date", "Open", "Close"])
+    # stock_amzn = pd.read_csv('stock_data/raw/AMZN.csv')
+    # stock_orcl = pd.read_csv('stock_data/ORCL.csv')
+    # display(stock_aapl)
+    #
+    # stock_aapl["closeMinusOpen"] = calulateOpenClose(stock_aapl)
+    # print(stock_aapl)
